bf14e63b8571add767f88a2cc6c8e4ccf4d2c987
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-06-09 23:10:24 -0600

    Added basic layout to about page

--- api/controllers/admin.js
+++ api/controllers/admin.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
+const Admin = require('../models/Admin');
 
 exports.admin_login = (req, res, next ) => {
-    res.json({msg: "Admin has logined in"});
+    // res.json({msg: "Admin has logined in"});
+    Admin.findOne({user: "lmanzanero"}, function(err, user) {
+        res.send(user);
+      });
 }
--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -2,8 +2,35 @@ import React from 'react';
 
 const About = () =>{
   return (
-    <div>
-        About
+    <div className="about-page">
+        <h1>Who am I?</h1>
+        <p>Hi I'm Jackson Ford On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country.</p>
+        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+
+        <div className="flex-container">
+          <div className="services">
+            <icon>Icon</icon>
+            <h3>Software Development</h3>
+          </div>
+          <div className="services">
+            <icon>Icon</icon>
+            <h3>Web Design</h3>
+          </div>
+          <div className="services">
+            <icon>Icon</icon>
+            <h3>Web Design</h3>
+          </div>
+          <div className="services">
+            <icon>Icon</icon>
+            <h3>Mobile Development</h3>
+          </div>
+        </div>
+
+        <div className="flex-container about-projects">
+            <p>I am happy to know you 
+                that 300+ projects done sucessfully!</p>
+            <button>Hire me</button>
+        </div>
     </div>
   );
 }
