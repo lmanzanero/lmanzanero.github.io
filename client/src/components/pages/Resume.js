a449d0744a183412a5afdaf935f35cabb20b456d
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-08-06 21:44:32 -0600

    Added basic layout for item in resume

--- client/src/components/pages/Resume.js
+++ client/src/components/pages/Resume.js
@@ -6,11 +6,13 @@ const Resume = () =>{
         <h1>Resume</h1>
         <div className="work-section">
           <article className="work-item">
-            <div className="timeline-icon">
-              <i>ICON</i>
+            <div className="work-item-inner">
+                <div className="timeline-icon">
+                  <i>ICON</i>
+                </div>
+                <h3>Full Stack Web Developer at Belizing.com<span> 2018-present</span></h3>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
             </div>
-            <h3>Full Stack Web Developer at Belizing.com<span> 2018-present</span></h3>
-            <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
           </article>
         </div>
     </div>
