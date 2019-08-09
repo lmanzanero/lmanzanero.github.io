e2cc63d1baf370e949578cfba53aab0e0496cd0e
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-08-08 22:53:31 -0600

    finished styling work experience item section

--- client/src/components/pages/Resume.js
+++ client/src/components/pages/Resume.js
@@ -3,17 +3,46 @@ import React from 'react';
 const Resume = () =>{
   return (
     <div className="resume-page">
-        <h1>Resume</h1>
+        <div className="resume-header">
+          <h1><i>Icon </i>Work Experience</h1>
+        </div>
         <div className="work-section">
           <article className="work-item">
             <div className="work-item-inner">
-                <div className="timeline-icon">
-                  <i>ICON</i>
+                <div className="work-logo">
+                  <img alt="Belizing.com" src="https://di7b4gw2u10mc.cloudfront.net/295000/294288.906.486da78a22.jpg"/>
                 </div>
-                <h3>Full Stack Web Developer at Belizing.com<span> 2018-present</span></h3>
+                <div className="work-heading">
+                  <h2>Full Stack Web Developer at Belizing.com</h2>
+                  <span>2018-present</span>
+                </div>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
                 <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
             </div>
           </article>
+        </div>
+        <div className="work-section">
+          <article className="work-item">
+            <div className="work-item-inner">
+                <div className="work-logo">
+                  <img alt="Belizing.com" src="https://di7b4gw2u10mc.cloudfront.net/295000/294288.906.486da78a22.jpg"/>
+                </div>
+                <div className="work-heading">
+                  <h2>Full Stack Web Developer at Belizing.com</h2>
+                  <span>2018-present</span>
+                </div>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+            </div>
+          </article>
+        </div>
+        <div className="resume-header">
+          <h1><i>Icon </i>Volunteer Work Experience</h1>
+        </div>
+        <div className="resume-header">
+          <h1><i>Icon </i>Education</h1>
         </div>
     </div>
   );
