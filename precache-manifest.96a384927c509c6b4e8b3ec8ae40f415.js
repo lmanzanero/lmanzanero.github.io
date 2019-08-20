8e121ab6fa55c48d62be532298f58aa948c1749b
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-08-19 22:15:01 -0600

    Updates

---
+++ precache-manifest.96a384927c509c6b4e8b3ec8ae40f415.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c514951e11826f5af8e050485446104c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1934f7d063e88d72f0a3",
+    "url": "/portfolio/static/css/main.6d5f3c29.chunk.css"
+  },
+  {
+    "revision": "46ff8eae5ad2b94771a9",
+    "url": "/portfolio/static/js/2.3d78f23e.chunk.js"
+  },
+  {
+    "revision": "1934f7d063e88d72f0a3",
+    "url": "/portfolio/static/js/main.0ceb43bd.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
