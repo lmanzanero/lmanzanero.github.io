5a9f995c2d4929547d8b281ff341cc2a79b0ade4
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-08-21 21:10:15 -0600

    added dummy text to sections

--- client/src/components/pages/Resume.js
+++ client/src/components/pages/Resume.js
@@ -47,8 +47,40 @@ const Resume = () =>{
         <div className="resume-header">
           <h1><i>Icon </i>Volunteer Work Experience</h1>
         </div>
+        <div className="work-section">
+          <article className="work-item">
+            <div className="work-item-inner">
+                <div className="work-logo">
+                  <img alt="Belizing.com" src="https://di7b4gw2u10mc.cloudfront.net/295000/294288.906.486da78a22.jpg"/>
+                </div>
+                <div className="work-heading">
+                  <h2>Full Stack Web Developer at Belizing.com</h2>
+                  <span>2018-present</span>
+                </div>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+            </div>
+          </article>
+        </div>
         <div className="resume-header">
           <h1><i>Icon </i>Education</h1>
+        </div>
+        <div className="work-section">
+          <article className="work-item">
+            <div className="work-item-inner">
+                <div className="work-logo">
+                  <img alt="Belizing.com" src="https://di7b4gw2u10mc.cloudfront.net/295000/294288.906.486da78a22.jpg"/>
+                </div>
+                <div className="work-heading">
+                  <h2>Full Stack Web Developer at Belizing.com</h2>
+                  <span>2018-present</span>
+                </div>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+            </div>
+          </article>
         </div>
     </div>
   );
