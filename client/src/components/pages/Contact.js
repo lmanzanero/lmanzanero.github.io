fe38eb8cdec138dd041dc891a66a4948db0f8840
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-08-22 22:43:27 -0600

    added contact page and offset navbar for mobile devices

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -2,8 +2,46 @@ import React from 'react';
 
 const Contact = () =>{
   return (
-    <div>
-        Contact 
+    <div className="contact-page">
+        <h1 className="brand"><span>Lets Work Together</span></h1>
+        <div className="wrapper animated bounceInLeft">
+          <div className="company-info">
+              <h3>Contact me</h3>
+              <ul>
+                <li><i className="fa fa-road"></i> 44 Something st</li>
+                <li><i className="fa fa-phone"></i> (555) 555-5555</li>
+                <li><i className="fa fa-envelope"></i> test@acme.test</li>
+              </ul>
+          </div>
+          <div className="contact">
+              <h3>Email Us</h3>
+              <form>
+                <p>
+                  <label>Name</label>
+                  <input type="text" name="name"/>
+                </p>
+                <p>
+                  <label>Company</label>
+                  <input type="text" name="company"/>
+                </p>
+                <p>
+                  <label>Email Address</label>
+                  <input type="email" name="email"/>
+                </p>
+                <p>
+                  <label>Phone Number</label>
+                  <input type="text" name="phone"/>
+                </p>
+                <p className="full">
+                  <label>Message</label>
+                  <textarea name="message" rows="5"></textarea>
+                </p>
+                <p className="full">
+                  <button>Submit</button>
+                </p>
+              </form>
+          </div>
+        </div>
     </div>
   );
 }
