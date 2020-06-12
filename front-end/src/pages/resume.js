a2feaec5543ecdc87f3497bb2ea23dc6fd78ab03
Luis Manzanero <manzanero.luis1995@gmail.com>
2020-06-13 01:40:18 +0800

    Added other pages to site from previous website

---
+++ front-end/src/pages/resume.js
@@ -0,0 +1,89 @@
+import React from 'react';  
+import { Helmet } from 'react-helmet';
+
+const Resume = () =>{
+  return (
+    <div className="resume page">
+        <Helmet>
+          <title>Resume | Luis Manzanero</title>
+          <meta name="description" content="I have a bachelors Degree in Enviornmetal Science and 5+ years as a software developer" />
+          <meta name="keywords" content="Bachelors Degree, GIS Specialist, Data Scientist, Web Developer, Guitarist, Adenturist, Innovator, Software Developer" />
+        </Helmet>
+        <div className="resume-header">
+          <h1><i> Icon </i>Work Experience</h1>
+        </div>
+        <div className="work-section">
+          <article className="work-item">
+            <div className="work-item-inner">
+                <div className="work-logo">
+                  <img alt="Belizing.com" src="https://di7b4gw2u10mc.cloudfront.net/295000/294288.906.486da78a22.jpg"/>
+                </div>
+                <div className="work-heading">
+                  <h2>Full Stack Web Developer at Belizing.com</h2>
+                  <span>2018-present</span>
+                </div>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+            </div>
+          </article>
+        </div>
+        <div className="work-section">
+          <article className="work-item">
+            <div className="work-item-inner">
+                <div className="work-logo">
+                  <img alt="Belizing.com" src="https://di7b4gw2u10mc.cloudfront.net/295000/294288.906.486da78a22.jpg"/>
+                </div>
+                <div className="work-heading">
+                  <h2>Full Stack Web Developer at Belizing.com</h2>
+                  <span>2018-present</span>
+                </div>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+            </div>
+          </article>
+        </div>
+        <div className="resume-header">
+          <h1><i>Icon </i>Volunteer Work Experience</h1>
+        </div>
+        <div className="work-section">
+          <article className="work-item">
+            <div className="work-item-inner">
+                <div className="work-logo">
+                  <img alt="Belizing.com" src="https://di7b4gw2u10mc.cloudfront.net/295000/294288.906.486da78a22.jpg"/>
+                </div>
+                <div className="work-heading">
+                  <h2>Full Stack Web Developer at Belizing.com</h2>
+                  <span>2018-present</span>
+                </div>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+            </div>
+          </article>
+        </div>
+        <div className="resume-header">
+          <h1><i>Icon </i>Education</h1>
+        </div>
+        <div className="work-section">
+          <article className="work-item">
+            <div className="work-item-inner">
+                <div className="work-logo">
+                  <img alt="Belizing.com" src="https://di7b4gw2u10mc.cloudfront.net/295000/294288.906.486da78a22.jpg"/>
+                </div>
+                <div className="work-heading">
+                  <h2>Full Stack Web Developer at Belizing.com</h2>
+                  <span>2018-present</span>
+                </div>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+                <p>Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees but rooms think may.</p>
+            </div>
+          </article>
+        </div>
+    </div>
+  );
+}
+
+export default Resume;
