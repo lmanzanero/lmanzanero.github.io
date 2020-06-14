0b32b011aaeb818229b976750e18b25922f494fe
Luis Manzanero <manzanero.luis1995@gmail.com>
2020-06-15 00:41:23 +0800

    Upgraded color codes and added active state to current project category

--- front-end/src/pages/projects.js
+++ front-end/src/pages/projects.js
@@ -1,18 +1,35 @@
-import React from 'react';  
+import React, { useState }from 'react';  
 import Layout from '../layouts/layout';
 
 const Projects = () =>{
+  const [isActive, setIsActive] = useState(0);
+  // fetch('http://localhost:3000/api/projects/', {
+  //   method: 'GET',
+  //   mode:'no-cors'
+  // })
+  // .then(res => {
+  //   console.log(res.text)
+  // }) 
+
+  const onClick = (e) => { 
+    const options = document.querySelectorAll('.option'); 
+    options.forEach(option => { 
+      option.classList.remove('active');
+    });
+    e.target.classList.add('active');
+  }
+
   return (
     <Layout>
         <div className="page">
           <h1>My Recent Work</h1>
           <div className="projects-options">
                 <ul>
-                  <li><a className="option" href="#">All</a></li>
-                  <li><a className="option active" href="#">Web Design</a></li>
-                  <li><a className="option" href="#">Mobile Development</a></li>
-                  <li><a className="option" href="#">Videos</a></li>
-                  <li><a className="option" href="#">Music</a></li>   
+                  <li onClick={onClick}><a className="option active" href="#">All</a></li>
+                  <li onClick={onClick}><a className="option" href="#">Web Design</a></li>
+                  <li onClick={onClick}><a className="option" href="#">Mobile Development</a></li>
+                  <li onClick={onClick}><a className="option" href="#">Videos</a></li>
+                  <li onClick={onClick}><a className="option" href="#">Music</a></li>   
                 </ul>
           </div>
           <div className="project-items">
