17249b5b4c6620a7e2c665602e2a964679de6d0c
Luis Manzanero <manzanero.luis1995@gmail.com>
2020-08-18 13:34:14 +0800

    added svg icons to home page

---
+++ front-end/src/pages/app.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Router } from "@reach/router" 
+import PrivateRoute from "../components/PrivateRoute" 
+import Dashboard from "./dashboard"
+import Layout from "../layouts/layout"
+import Login from "./login"
+
+const App = () => (
+  <Layout>  
+    <Router> 
+      <PrivateRoute path="/app/dashboard" component={Dashboard} />
+      <Login path="/app/login" />
+    </Router>
+  </Layout>
+)
+
+export default App
